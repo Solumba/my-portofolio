d6cfe167aaa169242947cfbbd56b27165e380ee3
Olumba solomon <olumbasolomon08@gmail.com>
2022-03-22 20:23:49 +0100

    Basic Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Header, Contact, Footer, About, Experience, Testimonials, Nav, Portofolio, Services} from '../src/components/ComponentExports'
+const App = () => {
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
