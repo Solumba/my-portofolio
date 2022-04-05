73b2e0c0e4d511a71aa88c995366aa05aa565ae0
Olumba solomon <olumbasolomon08@gmail.com>
2022-04-05 15:56:54 +0100

    working on portofolio

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,8 +1,76 @@
 import React from 'react';
 import './portofolio.css';
+import IMG from '../../assets/data.jpg';
 const Portofolio = () => {
   return (
-    <section id='portofolio'>Portofolio</section>
+    <section id='portofolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portofolio</h2>
+
+      <div className="container portofolio__container">
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        <article className="portofolio__item">
+          <div className="portofolio__item-image">
+            <img src={IMG} alt="data image" />
+          </div>
+          <h3>This is a portofolio item title</h3>
+          <div className="portofolio__item-cta">
+            <a href="https://github.com/Solumba" target="_blank" className="btn">Github</a>
+            <a href="https://behance.com/olumbasolomon" className="btn btn-primary">Live Demo</a>
+          </div>
+        </article>
+        
+      </div>
+    </section>
   )
 }
 
